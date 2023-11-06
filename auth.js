663edbd65696893b70a2f18051291296b4f267c9
hare32 <234694@edu.p.lodz.pl>
2023-11-06 17:49:40 +0100

    zmiana suth

--- auth.js
+++ auth.js
@@ -2,24 +2,41 @@ const msalConfig = {
     auth: {
         clientId: "d3a8cec7-9a81-4676-bf1f-4b67735d63dc",
         authority: "https://login.microsoftonline.com/67ea5955-9b5c-4693-a8f9-960f2a3b49bb",
-        redirectUri: "http://localhost:3000/dashboard.html",
+        redirectUri: "https://hare32.github.io/CloudApp/dashboard.html", // Zaktualizuj to do twojego publicznego URL
     }
 };
 
 const myMSALObj = new msal.PublicClientApplication(msalConfig);
 
 function signIn() {
-    myMSALObj.loginPopup({
+    myMSALObj.loginRedirect({
         scopes: ["user.read"],
         prompt: "select_account"
-    }).then((loginResponse) => {
-        // Możesz teraz przekierować użytkownika do dashboard.html
-        window.location.href = 'dashboard.html';
-    }).catch((error) => {
-        console.error(error);
     });
+    // Nie ma potrzeby używania .then() po loginRedirect
 }
+
+function handleResponse(loginResponse) {
+    if (loginResponse !== null) {
+        // Zalogowany użytkownik, przekierowanie do dashboard.html
+        window.location.href = 'dashboard.html';
+    } else {
+        // Użytkownik nie jest zalogowany, możesz wyświetlić interfejs logowania
+    }
+}
+
+myMSALObj.handleRedirectPromise().then(handleResponse).catch((error) => {
+    console.error(error);
+});
 
 function signOut() {
     myMSALObj.logout();
+}
+
+// Wywołaj handleResponse na początku, aby obsłużyć przypadki, gdy użytkownik wraca po przekierowaniu
+if (window.location.href.indexOf("dashboard.html") === -1) {
+    // Tylko jeśli nie jesteśmy już na stronie dashboard.html
+    myMSALObj.handleRedirectPromise().then(handleResponse).catch((error) => {
+        console.error(error);
+    });
 }
